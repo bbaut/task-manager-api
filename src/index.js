3ceecc2cc24210477217bdd2b37b6740575bb682
BranBautista <bbrandon.abo@gmail.com>
2023-09-02 00:10:38 -0600

    Added a logger

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import express, { json } from "express";
 import './db/mongoose.mjs';
 import * as userRouter from "./routers/user.js";
 import * as taskRouter from "./routers/task.js";
-// import bcrypt from 'bcryptjs'
-// import jwt from 'jsonwebtoken';
+import * as expressWinston from "express-winston";
+import logger from "./logger/logger.js";
 
 const app = express ();
 const port = process.env.PORT;
@@ -11,6 +11,12 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(userRouter.router);
 app.use(taskRouter.router);
+app.use(
+    expressWinston.logger({
+      winstonInstance: logger,
+      statusLevels: true,
+    })
+  );
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
---
+++ src/logger/logger.js
@@ -0,0 +1,35 @@
+import { createLogger, transports, format } from "winston";
+const { combine, printf } = format;
+
+const myFormat = printf(({ level, message, timestamp, ...metadata }) => {
+    let log = `${timestamp}[${level}]: ${message}`;
+
+    if (metadata) {
+        log += JSON.stringify(metadata);
+    }
+
+    return log;
+});
+
+const logger = createLogger({
+    transports: [
+        new transports.Console(),
+        new transports.File({
+            level: "warn",
+            filename: "./logs/logsWarnings.log"
+        }),
+        new transports.File({
+            level: "error",
+            filename: "./logs/logsErrors.log"
+        }),
+    ],
+
+    format: combine(
+        format.json(),
+        format.timestamp(),
+        format.colorize(),
+        myFormat
+    ),
+});
+
+export default logger;
